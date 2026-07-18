ca18fb06afce80dc687642fba64f21ef4de45b8a
Arjun Aarya <70001557+arjun2604@users.noreply.github.com>
2026-07-18 17:18:59 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -3,14 +3,21 @@
 
 int main()
 {
-    float PI = 3.147, r, A;
-    printf("Enter Radius");
-    scanf("%f", &r);
+    int P, R, T, SI;
 
-    A = PI * r * r;
+    printf("Enter Principle");
+    scanf("%i", &P);
 
-    printf("Area of circle is ");
-    printf("%f", A);
+    printf("Enter Rate");
+    scanf("%i", &R);
+
+    printf("Enter Time");
+    scanf("%i", &T);
+
+    SI = (P * R * T)/100;
+
+    printf("Simple Interest is ");
+    printf("%i", SI);
 
     return 0;
 }
