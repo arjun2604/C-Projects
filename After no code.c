6a53bc4dc61538fd1fb859ca8d0ef32c0b446fe9
Arjun Aarya <70001557+arjun2604@users.noreply.github.com>
2026-07-18 17:43:07 +0530

    Add program to increment a number by 1

---
+++ After no code.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int no, aftr;
+
+    printf("Pls enter the no \n");
+    scanf("%i", &no);
+
+    aftr = no + 1;
+
+    printf("After No is = %i", aftr);
+
+    return 0;
+}
