049e8e83f0c3613b7eaf1b154383b3858271b918
Arjun Aarya <70001557+arjun2604@users.noreply.github.com>
2026-07-18 23:54:53 +0530

    Add age eligibility check for voting

---
+++ Decision making 2.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int age;
+
+    printf("Enter age: ");
+    scanf("%d", &age);
+
+    if(age >= 18)
+    {
+        printf("Eligible to vote");
+
+    }
+    else
+    {
+        printf("Not eligible");
+    }
+    return 0;
+}
