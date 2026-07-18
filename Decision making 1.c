3dfc5a599213cb2404405730eff3f320cc28be97
Arjun Aarya <70001557+arjun2604@users.noreply.github.com>
2026-07-18 23:46:10 +0530

    Correct if statement syntax for positive number check
    Fixed syntax error in if statement to correctly check if the number is positive.

---
+++ Decision making 1.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int num;
+
+    printf("Enter a number: ");
+    scanf("%d", &num);
+
+    if(num > 0);
+    {
+        printf("Positive number");
+    }
+    return 0;
+}
